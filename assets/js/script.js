5791b0da034e07e7da37799ff11acb4d605e9c71
diarisdiakite <diarisdiakite@gmail.com>
2023-04-10 18:32:18 +0200

    Add Js Linters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const hamburgerNavMenu = document.querySelector(".hamburger-navmenu");
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerNavMenu = document.querySelector('.hamburger-navmenu');
 
-hamburgerMenu.addEventListener("click", function () {
-    hamburgerMenu.classList.toggle("active");
-    hamburgerNavMenu.classList.toggle("active");
-    //hamburgerNavMenu.classList.toggle("showHamburgerNavMenu");
-  });
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active');
+  hamburgerNavMenu.classList.toggle('active');
+  // hamburgerNavMenu.classList.toggle("showHamburgerNavMenu");
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", function () {
-    hamburgerMenu.classList.remove("active");
-    hamburgerNavMenu.classList.remove("active");
-    //hamburgerNavMenu.classList.remove("showHamburgerNavMenu");
-  }));
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburgerMenu.classList.remove('active');
+  hamburgerNavMenu.classList.remove('active');
+  // hamburgerNavMenu.classList.remove("showHamburgerNavMenu");
+}));
